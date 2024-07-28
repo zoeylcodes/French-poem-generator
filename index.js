b28abecf0550ba37add9ad649fb693f34af17268
Zoey <124487432+zoeylcodes@users.noreply.github.com>
2024-07-28 12:49:33 -0400

    Added API AI intergration

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-function generatePoem(event){
-    event.preventDefault();
-
-
+function displayPoem(response){
     new Typewriter('#poem', {
-      strings: ['Mon ami la rose..'],
+      strings: response.data.answer,
       autoStart: true,
       delay:1,
       cursor:"",
-    });
+    });}
+
+
+
+function generatePoem(event){
+event.preventDefault();
+
+let instructionsInput=document.querySelector("#user-instructions")
+let apiKey="28d6af13edado02eca594bd3ta001d5e";
+let prompt=`User instructions are: Generate a French Poem about ${instructionsInput.value}`;
+let context="You are a a French poet and you love to write short poems.Your mission is to generate a 4 line poem in basic HTML and separate each line with a </br>.Make sure you follow the user instructions.Do not include a title. Please sign the poem as 'SheCodes AI' inside a strong element like this <strong> SheCodes AI </strong> at the end of the poem and NOT at the begining";
+let apiURL=`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+axios.get(apiURL).then(displayPoem);
+
 
    
 }
